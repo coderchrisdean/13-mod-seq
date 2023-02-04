6de1888a8de03f8b47e596312e8d3a3100739b13
coderchrisdean <coderchrisdean@gmail.com>
2023-02-03 16:37:33 -0800

    added tables to models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,8 +6,42 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+    unique: true,
   },
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+    },
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10,
+    validate: {
+      isNumeric: true,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
+  },
+  },  
+  
   {
     sequelize,
     timestamps: false,
